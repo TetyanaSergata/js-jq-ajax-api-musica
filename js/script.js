7681237697f1cab5e66616715b91742a54598e77
Tetyana <tsergata@gmail.com>
2020-01-31 17:09:10 +0100

    chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert('ciao');
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+$(document).ready(function() {
+  var url = 'https://flynn.boolean.careers/exercises/api/array/music';
+
+  // Chiamata Ajax
+  $.ajax(
+    {
+      'url': url,
+      'method': 'Get',
+      'success': function(data) {
+      console.log(data);
+    },
+    'error': function (request, state, errors) {
+      alert('errore' + errors);
+    }
+  }
+  );
+
+
+});
